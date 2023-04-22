cda7ae2db6f49512a158134c4d287419c246de80
Dav-Sho <shodunkedavid39@gmail.com>
2023-04-23 01:30:23 +0200

    spring boot Security

--- src/main/java/com/sho/blog_api/Controller/PostController.java
+++ src/main/java/com/sho/blog_api/Controller/PostController.java
@@ -7,6 +7,7 @@ import com.sho.blog_api.Utils.PostResponse;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,6 +22,7 @@ public class PostController {
     }
 
 //    create post
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping()
     public ResponseEntity<PostDto> createPost(@Valid @RequestBody PostDto postDto){
         PostDto response = postService.createPost(postDto);
@@ -45,6 +47,7 @@ public class PostController {
     }
 
 //    Update Post
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/{id}")
     public ResponseEntity<PostDto> updatePost(@Valid @RequestBody PostDto postDto, @PathVariable long id){
         PostDto response = postService.updatePost(postDto, id);
@@ -52,6 +55,7 @@ public class PostController {
     }
 
 //    Delete Post
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deletePost(@PathVariable long id){
         String  response = postService.deletePost( id);
